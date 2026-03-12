113c0f8469d0fce86bc6dac447c622ef4a35394c
Naris404 <piotr.jakubczyk04@gmail.com>
2026-03-12 23:22:05 +0100

    Removed testing lines for unknown fields and renamed functionality file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from pypdf import PdfReader
 from time import sleep
 
-from fucntionality import (
+from functionality import (
     napraw_polskie_znaki,
     dane_firmy,
     informacje_faktury,
@@ -40,25 +40,18 @@ if uploaded_files is not None:
         txt = napraw_polskie_znaki(txt)
 
         inf_faktury = informacje_faktury(txt)
-        inf_faktury["nip"] = -1
         brakujace_pola = sprawdz_poprawnosc(inf_faktury)
         if brakujace_pola:
             for pole in brakujace_pola:
                 inf_faktury[pole] = st.text_input(f"Brakuje mi pola {pole}. Proszę uzupełnij:", key=f"input-{pole}")
-        print(inf_faktury["nip"])
         dane_firmowe = dane_firmy(txt)
         brakujace_pola = sprawdz_poprawnosc(dane_firmowe)
         if brakujace_pola:
             for pole in brakujace_pola:
                 dane_firmowe[pole] = st.text_input(f"Brakuje mi pola {pole}. Proszę uzupełnij:", key=f"input-{pole}")
+
         spis_towarow,konflikty = wczytywanie_listy_towarów_plubmer(txt)
-        brakujace_pola = sprawdz_poprawnosc(spis_towarow)
-        if brakujace_pola:
-            i = 0
-            for pole in brakujace_pola:
-                st.header(f"Pozycja numer {int(konflikty[i]) + 1}")
-                spis_towarow[int(konflikty[i]) + 1][pole] = st.text_input(f"Brakuje mi pola {pole}. Proszę uzupełnij:", key=f"input-{pole}")
-                i += 1
+
         nip = dane_firmowe["nip"]
 
         response = get(
@@ -72,7 +65,7 @@ if uploaded_files is not None:
 
         data = response.json()
 
-        plik_wyjsciowy = dane_do_xml(data, spis_towarow[0], inf_faktury)
+        plik_wyjsciowy = dane_do_xml(data, spis_towarow, inf_faktury)
 
         numer_fv = inf_faktury["numer_fv"].replace("/", "_")
         
